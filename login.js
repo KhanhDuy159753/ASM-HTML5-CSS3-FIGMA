8c57faaffb13b438a39805b14938890fc81a17a2
KhanhDuy159753 <125916124+KhanhDuy159753@users.noreply.github.com>
2023-06-20 09:25:16 +0700

    login.js

--- login.js
+++ login.js
@@ -4,3 +4,11 @@ function togglePassword() {
     if (check.checked) pass.type = "text";
     else pass.type = "password";
 }
+
+function saveData() {
+    var email = document.getElementById("Email").value;
+    var password = document.getElementById("pass").value;
+    localStorage.setItem("Email", email);
+    localStorage.setItem("pass", password);
+    alert("Đăng nhập thành công!");
+}
